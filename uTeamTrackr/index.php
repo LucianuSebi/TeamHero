f29ec440b8a85fe6ee93d185be85a66dcb23c90a
Bianca <149721587+bianca1901@users.noreply.github.com>
2024-02-27 16:28:00 +0200

    pagi8na de pornire

---
+++ uTeamTrackr/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/f3d0c2ca4c.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formular</title>
+    
+</head>
+
+
+<body>
+    <div class="topBar">
+        <div class="topSection">
+            
+        </div>
+
+        <div class="topSection">
+            
+        </div>
+    </div>
+    <div class="middleSection">
+
+    </div>
+    <div class="middleSection">
+
+    </div>
