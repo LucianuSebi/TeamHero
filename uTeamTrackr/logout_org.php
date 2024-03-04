bf55b4fcc263d2eb6d488325d47615e9761cec31
AlexandruEp <161039367+AlexandruEp@users.noreply.github.com>
2024-03-04 14:21:34 +0200

    update logout_org

--- uTeamTrackr/logout_org.php
+++ uTeamTrackr/logout_org.php
@@ -1,7 +1,7 @@
 <?php //deschidre php
 include "db_conn.php"; //conectam baza de date 
 session_start(); //Pornim sisiunea de logout
-ssion_unset(); //Ștergem datele care au rulat în sesiune
+session_unset(); //Ștergem datele care au rulat în sesiune
 session_destroy(); //Inchidem sesiune de logout
 header("location: ../login.php");  //Trimitere pagină de login
 exit(); //ieșire
