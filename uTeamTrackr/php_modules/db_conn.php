2651d8785c050d06ec4597220fb718fbf19a487b
LucianuSebi <127097933+LucianuSebi@users.noreply.github.com>
2024-03-08 23:35:52 +0200

    stilizari

--- uTeamTrackr/php_modules/db_conn.php
+++ uTeamTrackr/php_modules/db_conn.php
@@ -2,9 +2,9 @@
 
 //Conectare la baza de date
 
-$sname= "localhost";
-$uname= "root";
-$password= "";
+$sname = "localhost";
+$uname = "root";
+$password = "";
 $db_name = "uteamtrackr";
 
 //Functia mysqli_connect ia drept parametri numele serverului, username-ul, parola si numele bazei de date
--- uTeamTrackr/php_modules/login_user.php
+++ uTeamTrackr/php_modules/login_user.php
@@ -7,7 +7,7 @@ include "db_conn.php";
 
 
 //Verificare daca au fost trimise emailul si parola in campurile uEmail, respectiv uPass prin metoda POST
-if (isset($_POST['uEmail']) && isset($_POST['uPass'])){
+if (isset($_POST['uEmail']) && isset($_POST['uPass'])) {
 
     //Variabilele setate iau valorile din coloanele bazei de date
     $uEmail = mysqli_real_escape_string($conn, $_POST['uEmail']);
@@ -15,12 +15,12 @@ if (isset($_POST['uEmail']) && isset($_POST['uPass'])){
 
 
     //In caz de sunt campurile sunt goale va aparea eroarea respectiva
-    if(empty($uEmail) || empty($uPass)){
+    if (empty($uEmail) || empty($uPass)) {
 
         header("location: ../index.php?error=Email is required");
         exit();
 
-    }else{
+    } else {
 
         //Selectam din tabelul users din coloanele Email si parola si verificam unde valorile corespund cu cele introduse
         //Sql_result va contine randurile din tabel care satisfac conditiile interogarii
@@ -29,13 +29,13 @@ if (isset($_POST['uEmail']) && isset($_POST['uPass'])){
         $sql_result = mysqli_query($conn, $sql);
 
         //Daca functia returneaza doar un rand continuam
-        if(mysqli_num_rows($sql_result) === 1){
+        if (mysqli_num_rows($sql_result) === 1) {
 
             //Variabila row stocheaza sql_result drept un tablou 
             $row = mysqli_fetch_array($sql_result);
 
             //Variabila verificat indica faptul ca utilizatorul are contul verificat
-            if($row['Verified'] == '1'){
+            if ($row['Verified'] == '1') {
 
                 //Utlizatorul este autentificat
                 //Stocarea datelor intr-un tablou de sesiune numit user
@@ -46,25 +46,25 @@ if (isset($_POST['uEmail']) && isset($_POST['uPass'])){
                     'uEmail' => $row['Email'],
                     'uPhone' => $row['Phone'],
                     'rank' => $row['Rank'],
-                ]; 
+                ];
 
                 //Redirectionare la pagina principala
                 header("location: ../index.php");
                 exit();
 
-            }else{
-                
+            } else {
+
                 header("location: ../index.php?error=Please Verify your Email Adress.");
                 exit();
             }
 
-        }else{
+        } else {
             header("location: ../index.php?error=Incorect User Name or Password");
             exit();
         }
     }
 
-}else{
+} else {
     header("location: ../index.php?error=Please fill all fields");
     exit();
 }
