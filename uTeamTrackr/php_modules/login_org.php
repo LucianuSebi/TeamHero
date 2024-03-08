093a8bbb7a846f17a3a33314c8fe97989b08a451
LucianuSebi <127097933+LucianuSebi@users.noreply.github.com>
2024-03-08 23:01:05 +0200

    Imbunatatire login_org.php backend
    verifica daca org a fost verificata si ia mai multe informatii din baza de date

--- uTeamTrackr/php_modules/login_org.php
+++ uTeamTrackr/php_modules/login_org.php
@@ -2,48 +2,60 @@
 session_start();
 include "db_conn.php";
 
-if (isset($_POST['cEmail']) && isset($_POST['cPass']))
-{
+if (isset($_POST['cEmail']) && isset($_POST['cPass'])) {
     $cEmail = mysqli_real_escape_string($conn, $_POST['cEmail']);
     $cPass = mysqli_real_escape_string($conn, $_POST['cPass']);
 
-    if(empty($cEmail))
-    {
+    if (empty($cEmail)) {
         header("location: ../index.php?error = Email is required");
         exit();
-    } else if(empty($cPass))
-    {
+    } else if (empty($cPass)) {
         header("location: ../index.php?error = Password is required");
         exit();
-    }
-    else
-    {
+    } else {
         $sql = "SELECT * FROM users WHERE Email = '$cEmail' AND Pass = '$cPass'";
-        $sql_result = mysqli_query($conn,$sql);
-        if (mysqli_num_rows($sql_result) == 1)
-        {
-            $row = mysqli_fetch_array($sql_result);
-            if($row['Verified'] == '1')
-            {
-                $_SESSION['auth'] = TRUE;
-                $_SESSION['organization'] = [
-                    'cEmail' => $row['Email'],
-                ];
-                header("location: ../index.php");
-                exit();
-            }else
-            {
-                header("location: ../index.php?error=Please Verify your Email Adress.");
+        $sql_result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($sql_result) == 1) {
+            $urow = mysqli_fetch_array($sql_result);
+            if ($urow['Verified'] == 1) {
+
+                $orgID = $urow['Org'];
+
+                $sql = "SELECT * FROM organizations WHERE ID = 'orgID'";
+                $sql_result = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($sql_result) == 1) {
+
+                    $crow = mysqli_fetch_array($sql_result);
+
+                    if ($crow['Verified'] == '1') {
+                        $_SESSION['auth'] = TRUE;
+                        $_SESSION['organization'] = [
+                            'uEmail' => $urow['uEmail'],
+                            'cEmail' => $crow['Email'],
+                            'cName' => $crow['Name'],
+                        ];
+                        header("location: ../index.php");
+                        exit();
+                    } else {
+                        header("location: ../index.php?error=Please Verify Your Organization's Email Adress.");
+                        exit();
+                    }
+                } else {
+                    header("location: ../index.php?error=User is not affiliated to any organizations");
+                    exit();
+                }
+
+            } else {
+                header("location: ../index.php?error=Please Verify Your User Email Adress");
                 exit();
             }
-        } else
-        {
+        } else {
             header("location: ../index.php?error=Incorect Name or Password");
             exit();
         }
 
-        }
-    }else{
-        header("location: ../index.php");
-        exit();
     }
+} else {
+    header("location: ../index.php");
+    exit();
+}
