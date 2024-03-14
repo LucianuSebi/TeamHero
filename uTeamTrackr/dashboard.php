a612549b76f57b02dc6045ac3c1ad31bb1306f88
LucianuSebi <127097933+LucianuSebi@users.noreply.github.com>
2024-03-15 00:07:56 +0200

    Created dashboard page

---
+++ uTeamTrackr/dashboard.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/f3d0c2ca4c.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start-up Page</title>
+
+</head>
+
+
+<body>
+    <div class="leftBar">
+        <div class="logo">
+
+        </div>
+
+        <div class="category">
+
+            <div class="categoryHeader">
+                <p>PROJECTS</p>
+                <i class="fa-solid fa-angle-up"></i>
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#">Your Project</a>
+                </li>
+            </ul>
+
+        </div>
+        <div class="category">
+
+            <div class="categoryHeader">
+                <p>MANAGE</p>
+                <i class="fa-solid fa-angle-up"></i>
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#">Feeds</a>
+                </li>
+                <li>
+                    <a href="#">Your Account</a>
+                </li>
+                <li>
+                    <a href="#">Monitoring</a>
+                </li>
+                <li>
+                    <a href="#">Chats</a>
+                </li>
+            </ul>
+
+        </div>
+        <div class="category">
+
+            <div class="categoryHeader">
+                <p>ADMINISTRATION</p>
+                <i class="fa-solid fa-angle-up"></i>
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#">Members</a>
+                </li>
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+                <li>
+                    <a href="#">Organization</a>
+                </li>
+            </ul>
+
+        </div>
+
+        <div class="category" style="padding-top:20px;border-bottom:0px;">
+
+            <ul>
+                <li>
+                    <a href="">Support</a>
+                </li>
+                <li>
+                    <a href="">Settings</a>
+                </li>
+                <li>
+                    <a href="">What's New</a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+</body>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const buttons = document.querySelectorAll('.categoryHeader');
+
+        buttons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                const list = this.nextElementSibling;
+                if(list.style.height === ''){
+                    list.style.height = '0px';
+                }
+                else{
+                    list.style.height = '';
+                }
+            });
+        });
+    });
+</script>
