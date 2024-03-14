686ee81e1e3434047f437ad509b445adbbe79c39
Bianca <149721587+bianca1901@users.noreply.github.com>
2024-03-14 21:55:45 +0200

    Modified comments on db_conn.php file
    I changed the comments in the db_conn.php file from romanian into english

--- uTeamTrackr/php_modules/db_conn.php
+++ uTeamTrackr/php_modules/db_conn.php
@@ -1,6 +1,6 @@
 <?php
 
-//Conectare la baza de date
+//Connecting to data base
 
 require '../../vendor/autoload.php';
 
@@ -13,6 +13,6 @@ $uname= $_ENV['DATABASE_USERNAME'];
 $password= $_ENV['DATABASE_PASSWORD'];
 $db_name = $_ENV['DATABASE_NAME'];
 
-//Functia mysqli_connect ia drept parametri numele serverului, username-ul, parola si numele bazei de date
+//The functionmysqli_connect takes as parameters the name of the server, the username, the password and the name of the data base
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
