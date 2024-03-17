0aa0e10a688e685f6d9b6ddd45cf043e11a26ee4
Pamparau Victor <161034168+PilotViki@users.noreply.github.com>
2024-03-17 21:19:22 +0200

    add pass for database

---
+++ uTeamTrackr/index.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://kit.fontawesome.com/f3d0c2ca4c.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start-up Page</title>
+
+</head>
+
+<body>
+    <div class="topBar">  
+ <div class="topSection">
+
+
+ </div>
+ <div class="topSection">
+
+
+ </div>
+ <div class="topSection">
+
+
+ </div>
+ 
+    </div>
+    <div class="section">
+
+    </div>
+    <div class="section">
+        
+    </div>
+    <div class="section">
+        
+    </div>
+    <div class="bottomBar">
+
+
+    </div>
+</body>
