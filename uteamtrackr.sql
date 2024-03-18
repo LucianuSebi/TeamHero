d2f8255b8e02200b5bb1f7eb651cd1db2d98f931
LucianuSebi <127097933+LucianuSebi@users.noreply.github.com>
2024-03-18 05:05:49 +0200

    uploaded the new database

--- uteamtrackr.sql
+++ uteamtrackr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2024 at 10:00 PM
+-- Generation Time: Mar 17, 2024 at 10:30 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,6 +38,19 @@ CREATE TABLE `departaments` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `endorsements`
+--
+
+CREATE TABLE `endorsements` (
+  `ID` int(255) NOT NULL,
+  `Skill` int(255) NOT NULL,
+  `Sender` int(255) NOT NULL,
+  `Recipient` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `organizations`
 --
 
@@ -57,7 +70,7 @@ CREATE TABLE `organizations` (
 --
 
 INSERT INTO `organizations` (`ID`, `Name`, `Phone`, `Email`, `Adress`, `Admin`, `Token`, `Verified`) VALUES
-(6, 'Sebastian-Lucian Amariei', '+40771798737', 'strugar.sebastian@gmail.com', 'Str. Grigore Antipa nr.9,', 0, '78e63f5edd6d3f48377f7bedddd9aa64', 0);
+(12, 'Sebastian-Lucian Amariei', '+40771798737', 'amariei_sebastianl@yahoo.com', 'Str. Grigore Antipa nr.9,', 0, '2ae628d7c42045d37ec7de9b7315e156', 1);
 
 -- --------------------------------------------------------
 
@@ -69,8 +82,37 @@ CREATE TABLE `projects` (
   `ID` int(255) NOT NULL,
   `Name` varchar(255) NOT NULL,
   `Dept` int(255) NOT NULL,
+  `Org` int(255) NOT NULL,
   `Users` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`ID`, `Name`, `Dept`, `Org`, `Users`) VALUES
+(1, 'Gasirea Timpului', 1, 12, ''),
+(2, 'Gasirea Timpului', 1, 12, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `ID` int(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Org` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `skills`
+--
+
+INSERT INTO `skills` (`ID`, `Name`, `Org`) VALUES
+(1, 'PHP', 12),
+(3, 'JAVA', 12);
 
 -- --------------------------------------------------------
 
@@ -84,14 +126,39 @@ CREATE TABLE `users` (
   `LName` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Phone` varchar(255) NOT NULL,
+  `Country` varchar(255) NOT NULL,
+  `County` varchar(255) NOT NULL,
+  `City` varchar(255) NOT NULL,
+  `PostalCode` varchar(255) NOT NULL,
   `Pass` varchar(255) NOT NULL,
   `Rank` varchar(255) NOT NULL,
+  `Bio` text NOT NULL,
+  `Img` varchar(255) NOT NULL,
   `Skills` varchar(255) NOT NULL,
   `Org` int(255) NOT NULL,
   `Dept` int(255) NOT NULL,
   `Projects` varchar(255) NOT NULL,
   `Token` varchar(255) NOT NULL,
   `Verified` int(255) NOT NULL DEFAULT 0 COMMENT '0=Unverified'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `FName`, `LName`, `Email`, `Phone`, `Country`, `County`, `City`, `PostalCode`, `Pass`, `Rank`, `Bio`, `Img`, `Skills`, `Org`, `Dept`, `Projects`, `Token`, `Verified`) VALUES
+(25, 'Sebastian-Lucian', 'Amariei', 'amariei_sebastianl@yahoo.com', '+40771798737', 'Romania', 'Suceava', 'Suceava', '720111', 'd', 'admin', 'asdasdasd', '', 'a:4:{i:0;s:1:\"3\";i:1;s:1:\"5\";i:2;s:1:\"6\";i:3;s:1:\"1\";}', 12, 0, '', '2e331506c7393b05d9417f28b477f693', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verified_skills`
+--
+
+CREATE TABLE `verified_skills` (
+  `ID` int(255) NOT NULL,
+  `Skill` int(255) NOT NULL,
+  `Recipient` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -102,6 +169,12 @@ CREATE TABLE `users` (
 -- Indexes for table `departaments`
 --
 ALTER TABLE `departaments`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `endorsements`
+--
+ALTER TABLE `endorsements`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -117,9 +190,21 @@ ALTER TABLE `projects`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `verified_skills`
+--
+ALTER TABLE `verified_skills`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -133,22 +218,40 @@ ALTER TABLE `departaments`
   MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `endorsements`
+--
+ALTER TABLE `endorsements`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `organizations`
 --
 ALTER TABLE `organizations`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `verified_skills`
+--
+ALTER TABLE `verified_skills`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
